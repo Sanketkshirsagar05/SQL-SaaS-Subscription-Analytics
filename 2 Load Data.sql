a56ab91ef3f8599956ecffbff930dabd8189916e
Sanket Kshirsagar <102159978+Sanketkshirsagar05@users.noreply.github.com>
2025-12-27 18:49:20 +0530

    Add files via upload

---
+++ 2 Load Data.sql
@@ -0,0 +1,17 @@
+﻿USE SaaS_Subscription_Analytics;
+GO
+
+--  Master / Dimension tables first
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\plans.sql"
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\users.sql"
+
+--  Core transactional tables
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\subscriptions.sql"
+
+--  Dependent tables
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\payments.sql"
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\cancellations.sql"
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\plan_changes.sql"
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\usage_logs.sql"
+:r "E:\Desktop\Excelr\Projects\DA Project\SQL\5 SQL SaaS Subscription Analytics\Data\support_tickets.sql"
+GO
