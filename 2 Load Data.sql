14329e549b6407fd995408c7daedac85cc92b04c
Sanket Kshirsagar <102159978+Sanketkshirsagar05@users.noreply.github.com>
2025-12-28 11:25:07 +0530

    Add files via upload

---
+++ 2 Load Data.sql
@@ -0,0 +1,17 @@
+﻿USE SaaS_Subscription_Analytics;
+GO
+
+-- Master / Dimension tables
+:r "...\\Data\\plans.sql"
+:r "...\\Data\\users.sql"
+
+-- Core transactional tables
+:r "...\\Data\\subscriptions.sql"
+
+-- Dependent tables
+:r "...\\Data\\payments.sql"
+:r "...\\Data\\cancellations.sql"
+:r "...\\Data\\plan_changes.sql"
+:r "...\\Data\\usage_logs.sql"
+:r "...\\Data\\support_tickets.sql"
+GO
