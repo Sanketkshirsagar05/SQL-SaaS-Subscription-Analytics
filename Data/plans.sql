cf52caede01ce9caa7c32ec8702dafee7eb2b142
Sanket Kshirsagar <102159978+Sanketkshirsagar05@users.noreply.github.com>
2025-12-28 11:13:45 +0530

    Add files via upload

--- Data/plans.sql
+++ Data/plans.sql
@@ -1,4 +1,4 @@
-insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (1, 'Free', 'Monthly', '0', false);
-insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (2, 'Basic', 'Monthly', '499', true);
-insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (3, 'Pro', 'Monthly', '1499', true);
-insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (4, 'Enterprise', 'Monthly', '4999', true);
+insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (1, 'Free', 'Monthly', '0', 0);
+insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (2, 'Basic', 'Monthly', '499', 1);
+insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (3, 'Pro', 'Monthly', '1499', 1);
+insert into plans (plan_id, plan_name, billing_cycle, price, is_active) values (4, 'Enterprise', 'Monthly', '4999', 1);
